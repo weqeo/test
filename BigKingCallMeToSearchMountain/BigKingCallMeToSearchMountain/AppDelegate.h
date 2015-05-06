d29d45c5d6b9e8cac1d6730bccbf2f157b2e6ba7
weqeo <weqeo1989@gmail.com>
2015-05-06 11:34:29 +0800

    添加“大王叫我去巡山”

---
+++ BigKingCallMeToSearchMountain/BigKingCallMeToSearchMountain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BigKingCallMeToSearchMountain
+//
+//  Created by 魏强 on 15/5/6.
+//  Copyright (c) 2015年 魏强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BigKingCallMeToSearchMountain/BigKingCallMeToSearchMountain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BigKingCallMeToSearchMountain
+//
+//  Created by 魏强 on 15/5/6.
+//  Copyright (c) 2015年 魏强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
